1136e0a9b782b7186b3ae2210b48d796dd6ddeb0
David Arruda <deh0_@hotmail.com>
2021-03-07 22:36:23 -0300

    Modificado parametros recebidos pelos métodos, adicionado método calculateFreight

--- game-store/src/main/java/com/david/gamestore/service/SalesService.java
+++ game-store/src/main/java/com/david/gamestore/service/SalesService.java
@@ -1,33 +1,54 @@
 package com.david.gamestore.service;
 
-import com.david.gamestore.model.Product;
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.david.gamestore.model.Sale;
+import com.david.gamestore.model.SaleItem;
 
 /**
  * Serviço responsável por realizar as rotinas relacionadas com as vendas
+ * 
  * @author david
  *
  */
+@Service
 public interface SalesService {
-	
+
 	/**
 	 * Adiciona um novo produto no carrinho de compras
-	 * @param product
+	 * 
+	 * @param Sale, SaleItem
 	 * @throws Exception
 	 */
-	void addItem(Product product) throws Exception;
-	
+	void addItem(Sale sale, SaleItem saleItem) throws Exception;
+
 	/**
 	 * Remove um produto do carrinho de compra
-	 * @param product
+	 * 
+	 * @param Sale, SaleItem
 	 * @throws Exception
 	 */
-	void removeItem(Product product) throws Exception;
-	
+	void removeItem(Sale sale, SaleItem saleItem) throws Exception;
+
 	/**
 	 * Obtem o checkout(frete, subtotal e total) de uma venda.
-	 * @param product
+	 * 
+	 * @param sale
+	 * @return List<BigDecimal>
 	 * @throws Exception
 	 */
-	void getCheckOut(Product product) throws Exception;
-	
+	List<BigDecimal> getCheckOut(Sale sale) throws Exception;
+
+	/**
+	 * Calcula o frete relacionado a venda.
+	 * 
+	 * @param saleItems
+	 * @return BigDecimal
+	 * @throws Exception
+	 */
+	BigDecimal calculateFreight(List<SaleItem> saleItems) throws Exception;
+
 }
